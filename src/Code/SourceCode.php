f2f306acb242b30d0861d698a6b59f29b5db3e25
USAMI Kenta <tadsan@zonu.me>
2020-01-20 21:38:30 +0900

    Add Code interface and SourceCode class

---
+++ src/Code/SourceCode.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Playground\Code;
+
+use Playground\Code as CodeInterface;
+
+final class SourceCode implements CodeInterface
+{
+    private string $source;
+
+    public function __construct(string $source)
+    {
+        $this->source = $source;
+    }
+
+    public function __toString(): string
+    {
+        return $this->source;
+    }
+}
