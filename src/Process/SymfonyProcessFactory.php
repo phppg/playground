ef33be0939fe25a3d4d1d9029072682986a78fb6
USAMI Kenta <tadsan@zonu.me>
2020-01-20 21:38:08 +0900

    Add SymfonyProcessFactory class

---
+++ src/Process/SymfonyProcessFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Playground\Process;
+
+use Symfony\Component\Process\Process;
+
+final class SymfonyProcessFactory
+{
+    /**
+     * @param string[] $command The command to run and its arguments listed as separate entries
+     * @param ?string $cwd The working directory or null to use the working dir of the current PHP process
+     * @param ?array<string,string> $env The environment variables or null to use the same environment as the current PHP process
+     * @param ?mixed $input The input as stream resource, scalar or \Traversable, or null for no input
+     * @param ?float $timeout The timeout in seconds or null to disable
+     * @return Process<string,string>
+     */
+    public function create(array $command, string $cwd = null, array $env = null, $input = null, ?float $timeout = 60.0): Process
+    {
+        return new Process($command, $cwd, $env, $input, $timeout);
+    }
+}
